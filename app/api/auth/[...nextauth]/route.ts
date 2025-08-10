23817dc23c90819e99f7a74fbd9c428436b29867
Anghelescu Florin <florin-alexandru.anghelescu@s.unibuc.ro>
2025-08-11 01:00:15 +0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 export const runtime = "nodejs";          // <- important
 export const dynamic = "force-dynamic";   // avoid static optimization
+export const preferredRegion = ["iad1"];
 
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth-options";
